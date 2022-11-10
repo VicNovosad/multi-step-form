5091039b523c6dc4574d570c9a963adc05cd6004
Vic Novosad <vic.novosad.us@gmail.com>
2022-11-09 21:16:20 -0600

    refactoring buttonGroup with mapping

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -1,10 +1,23 @@
-export default function ButtonGroup({stage, setStage}) {
+import { useState } from "react";
+
+export default function ButtonGroup() {
+
+    const [selectedButton, setSelectedButton] = useState(0);
+
+    const BUTTON_TEXT = [
+        '$2k', '$5k', 'No limit'
+    ]
+    
     return (
-        <div className="button-group flex">
-            { stage !== 3 && <input 
-                onClick={() => setStage(stage + 1)}
-                type="submit" value="Next" />}
+        <div className="button-wrap flex">
+            {BUTTON_TEXT.map((value, index) => (
+                <input
+                    onClick={() => setSelectedButton(index)}
+                    className={selectedButton === index && "selected-button"}
+                    key={index}
+                    type="submit" value={value} />
+            ))}
         </div>
-    );
+    )
 }
   
